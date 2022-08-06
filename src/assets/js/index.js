291a3a72485c96ef674b531febc10c437e443a30
Sneik <Sneik92@ya.ru>
2022-08-06 15:53:45 +0300

    added pages

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -152,4 +152,70 @@ document.addEventListener("DOMContentLoaded", (e) => {
     },
   });
 
+  const steelFramesSwiper = new Swiper("#steel-frames__swiper", {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    spaceBetween: 21,
+    autoHeight: true,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+        slidesPerGroup: 1,
+      },
+      640: {
+        slidesPerView: 2.2,
+        spaceBetween: 10,
+        slidesPerGroup: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      }
+    },
+  });
+
+  const bvzSwiper = new Swiper("#bvz__swiper", {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    spaceBetween: 21,
+    autoHeight: true,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+        slidesPerGroup: 1,
+      },
+      640: {
+        slidesPerView: 2.2,
+        spaceBetween: 10,
+        slidesPerGroup: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      }
+    },
+  });
+
+  
+
 });
