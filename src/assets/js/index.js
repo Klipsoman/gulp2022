28c378978635741c7c8dfe93d900b4b3d881a18c
Sneik <Sneik92@ya.ru>
2022-08-10 17:17:35 +0300

    search.html added

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -8,9 +8,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const headerContent = header.querySelector(".header__content");
   const burgerBtn = header.querySelector("#burger-btn");
 
-  const innerBtn = document.querySelector('.inner__btn');
-  const modalForm = document.querySelector('#modal-form');
-  const closeModalFormBtn = document.querySelector('#modal-form .modal-form__close');
+  const innerBtn = document.querySelector(".inner__btn");
+  const modalForm = document.querySelector("#modal-form");
+  const closeModalFormBtn = document.querySelector(
+    "#modal-form .modal-form__close"
+  );
 
   document.addEventListener("scroll", (e) => {
     if (window.pageYOffset > 30) {
@@ -22,74 +24,94 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
   // Проверка устройства
   if (checkMobile.isMobile()) {
-    let menuItems = header.querySelectorAll('.header__navigation > ul > li');
+    let menuItems = header.querySelectorAll(".header__navigation > ul > li");
     let itemsFiltered = [...menuItems].filter((item) => {
       // есть ли подменю
       return item.children[1] ? true : false;
     });
 
     itemsFiltered.forEach((item) => {
-      item.classList.add('hasSubmenu');
-      item.addEventListener('click', (e) => {
-        item.classList.toggle('submenu-active');
-      })
-    })
+      item.classList.add("hasSubmenu");
+      item.addEventListener("click", (e) => {
+        item.classList.toggle("submenu-active");
+      });
+    });
 
-    document.body.classList.add('_mobile');
+    document.body.classList.add("_mobile");
   } else {
-    document.body.classList.add('_desktop');
+    document.body.classList.add("_desktop");
   }
 
-  // Mobile menu 
+  // Mobile menu
   burgerBtn.addEventListener("click", (e) => {
     if (headerContent.classList.contains("menuopen")) {
       burgerBtn.classList.remove("menuopen");
-      headerContent.classList.remove('menuopen');
+      headerContent.classList.remove("menuopen");
       document.body.style.overflowY = "";
       return;
     }
     burgerBtn.classList.add("menuopen");
-    headerContent.classList.add('menuopen');
+    headerContent.classList.add("menuopen");
     document.body.style.overflowY = "hidden";
   });
 
   // Sector market (change image by hover)
-  const sectorList = document.querySelector('.sector .sector__right .sector__list');
+  const sectorList = document.querySelector(
+    ".sector .sector__right .sector__list"
+  );
   if (sectorList) {
-    sectorList.addEventListener('mouseover', (e) => {
-      if (e.target.classList.contains('sector__link') || e.target.classList.contains('sector__item')) {
-        const item = e.target.tagName === 'LI' ? e.target : e.target.parentNode;
-        const images = document.querySelectorAll('.sector .sector__left .sector__image');
+    sectorList.addEventListener("mouseover", (e) => {
+      if (
+        e.target.classList.contains("sector__link") ||
+        e.target.classList.contains("sector__item")
+      ) {
+        const item = e.target.tagName === "LI" ? e.target : e.target.parentNode;
+        const images = document.querySelectorAll(
+          ".sector .sector__left .sector__image"
+        );
 
         for (let i = 0; i < images.length; i++) {
           if (images[i].dataset.num === item.dataset.num) {
-            images[i].dataset.visible = 'visible';
+            images[i].dataset.visible = "visible";
           } else {
-            images[i].dataset.visible = '';
-          };
+            images[i].dataset.visible = "";
+          }
         }
       }
-    })
+    });
   }
 
-  // Modal
+  // Modal. Home-page
   if (innerBtn) {
     innerBtn.addEventListener("click", (e) => {
-      modalForm.classList.add('modal-form__visible');
-    })
+      modalForm.classList.add("modal-form__visible");
+    });
 
-    closeModalFormBtn.addEventListener('click', closeModal)
+    closeModalFormBtn.addEventListener("click", closeModal);
 
     function closeModal() {
-      modalForm.classList.remove('modal-form__visible');
+      modalForm.classList.remove("modal-form__visible");
+    }
+  }
+  // Modal. 404.html
+  const btn404 = document.querySelector('.page404 .error404 button.error404__btn')
+  if (btn404) {
+    btn404.addEventListener("click", (e) => {
+      modalForm.classList.add("modal-form__visible");
+    });
+
+    closeModalFormBtn.addEventListener("click", closeModal);
+
+    function closeModal() {
+      modalForm.classList.remove("modal-form__visible");
     }
   }
 
-  // init sliders 
+  // init sliders
   const projectsSwiper = new Swiper("#projects__swiper", {
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     slidesPerView: 3,
     slidesPerGroup: 3,
@@ -123,8 +145,8 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
   const runSystemSwiper = new Swiper("#run-system__swiper", {
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     slidesPerView: 4,
     slidesPerGroup: 4,
@@ -149,14 +171,14 @@ document.addEventListener("DOMContentLoaded", (e) => {
       1400: {
         slidesPerView: 4,
         spaceBetween: 20,
-      }
+      },
     },
   });
 
   const steelFramesSwiper = new Swiper("#steel-frames__swiper", {
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     slidesPerView: 4,
     slidesPerGroup: 4,
@@ -181,14 +203,14 @@ document.addEventListener("DOMContentLoaded", (e) => {
       1400: {
         slidesPerView: 4,
         spaceBetween: 20,
-      }
+      },
     },
   });
 
   const bvzSwiper = new Swiper("#bvz__swiper", {
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     slidesPerView: 4,
     slidesPerGroup: 4,
@@ -213,11 +235,10 @@ document.addEventListener("DOMContentLoaded", (e) => {
       1400: {
         slidesPerView: 4,
         spaceBetween: 20,
-      }
+      },
     },
   });
 
   // Create maps
   createMaps();
-
 });
